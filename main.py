71a5f49ebdf1e0ab945737834bed26a5f11f4a72
Soare95 <soaremihai.a@yahoo.com>
2021-11-09 17:26:24 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from gtts import gTTS
+import PyPDF2
+
+PAGE_NUMBER = 35
+
+file = open("TR_SEA2_Geology.pdf", "rb")
+file_reader = PyPDF2.PdfFileReader(file)
+page = file_reader.getPage(PAGE_NUMBER)
+pdf_text = page.extractText()
+
+text_converter = gTTS(text=pdf_text)
+text_converter.save("output.mp3")
+
+
+
+
+
+
+
